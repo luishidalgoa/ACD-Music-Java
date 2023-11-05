d82ddaa8c13d1f0aeade352ce2821549c70a9e59
erich <erich.hamm21@gmail.com>
2023-11-05 21:47:08 +0100

    Song & SongDAO & Genre enum

--- src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/Enum/Genre.java
+++ src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/Enum/Genre.java
@@ -1,4 +1,20 @@
 package dev.iesfranciscodelosrios.acdmusic.Model.Enum;
 
 public enum Genre {
+    ROCK,
+    POP,
+    JAZZ,
+    HIP_HOP,
+    CLASSICAL,
+    ELECTRONIC,
+    COUNTRY,
+    RnB,
+    METAL,
+    REGGAE,
+    BLUES,
+    FUNK,
+    ALTERNATIVE,
+    INDIE,
+    LATIN,
+    OTHER
 }
