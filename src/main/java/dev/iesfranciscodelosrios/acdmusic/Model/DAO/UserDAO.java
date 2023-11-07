4f90862c190cb2d6e1d0911253e5fd085c25d13b
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-11-07 12:46:55 +0100

    Commit para poder hacer pull

--- src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/DAO/UserDAO.java
+++ src/main/java/dev/iesfranciscodelosrios/acdmusic/Model/DAO/UserDAO.java
@@ -8,7 +8,6 @@ import dev.iesfranciscodelosrios.acdmusic.Model.Domain.User;
 import java.sql.*;
 import java.util.Set;
 
-
 public class UserDAO extends User implements iUserDAO  {
     private static final String INSERT ="INSERT INTO user (name,email,picture,password,nickname,lastname) VALUES (?,?,?,?,?,?)";
     private static final String UPDATE ="UPDATE user SET name=?, email=?, picture=?, password=?, nickname=?, lastname=? WHERE id_user=?";
@@ -16,16 +15,39 @@ public class UserDAO extends User implements iUserDAO  {
     private static final String SELECTBYID ="SELECT id_user,name,email,picture,password,nickname,lastname FROM user WHERE id_user=?";
     private static final String SELECTALL ="SELECT id_user,name,email,picture,password,nickname,lastaname FROM user";
 
+    /**
+     * Metodo para setear un user con el que vamos a trabajar
+     * @param id_user id del user
+     * @param name nombre del user
+     * @param email email del user
+     * @param picture url de la imagen de perfil del user
+     * @param password contraseña del user
+     * @param lastname apellidos del user
+     */
     public UserDAO(int id_user, String name, String email, String picture, String password, String lastname) {
         super(id_user,name,email,picture,password,lastname);
     }
+
+    /**
+     * Metodo para obtener un user a través de su id
+     * @param id_user Id del user a busccar
+     */
     public UserDAO(int id_user){
         getById(id_user);
     }
+
+    /**
+     * Metodo para setear un user con el que vamos a trabajar y conocemos previamente
+     * @param u Usuario que se va a tratar en el DAO
+     */
     public UserDAO(User u){
         super(u.getId(), u.getName(), u.getEmail(), u.getPicture(), u.getPassword(), u.getNickName(), u.getLastName());
     }
 
+    /**
+     * Metodo para insertar un user en la base de datos
+     * @return true si la consulta SQL a tenido exito false si no ha tenido exito
+     */
     @Override
     public boolean addUser() {
         if(getId()!=-1){
@@ -61,6 +83,10 @@ public class UserDAO extends User implements iUserDAO  {
         }
     }
 
+    /**
+     * Metodo para actualizar un user de la base de datos
+     * @return true si la consulta SQL a tenido exito false si no ha tenido exito
+     */
     private boolean update() {
         if(getId() ==-1) return false;
         Connection conn = ConnectionData.getConnection();
@@ -82,6 +108,11 @@ public class UserDAO extends User implements iUserDAO  {
         }
     }
 
+    /**
+     * Metodo para obetener un usuario del que ya conocemos sus id
+     * @param id del usuari que queremos obtener
+     * @return si la consulta SQL a tenido exito false si no ha tenido exito
+     */
     public boolean getById(int id){
         Connection conn = ConnectionData.getConnection();
         if (conn==null) return false;
@@ -107,6 +138,11 @@ public class UserDAO extends User implements iUserDAO  {
         return true;
     }
 
+    /**
+     *
+     * @param idUser id del usuario a eliminar
+     * @return
+     */
     @Override
     public boolean delete(int idUser) {
         if(getId()==-1) return false;
