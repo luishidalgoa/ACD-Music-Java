b8213a7109bd63d713ce4f92c94d0d1f79efceb2
Luis <luishidalgoa@outlook.es>
2023-11-08 23:24:14 +0100

    Pausamos el desarrollo del componente para desarrollar MediaPlayer. Faltaria implementarle el play y el cargar la View en la minCard

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module dev.iesfranciscodelosrios.acdmusic {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
